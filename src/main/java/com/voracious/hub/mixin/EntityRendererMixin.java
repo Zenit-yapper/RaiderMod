e8bc7e921ba6e81764db10a30ecd7b93aae1d189
Zenit-yapper <zenitog69@gmail.com>
2026-03-28 07:10:42 +0530

    Update and rename EntityRendererMixin.java to EntityRendererMixin.java

---
+++ src/main/java/com/voracious/hub/mixin/EntityRendererMixin.java
@@ -0,0 +1,20 @@
+package com.voracious.hub.mixin;
+
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin<T extends Entity> {
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        // This is where the rank rendering logic will go
+    }
+}
